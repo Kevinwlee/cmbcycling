2e5d35dde9dc2f067da6105df4174d71e1bed472
Kevin W. Lee <kevin.lee@qtechnologycompany.com>
2011-10-21 22:03:11 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cmbcycling::Application.config.secret_token = 'e22448715a2b4794094107fcb556cd8adacc90b3f49ceb1ab35e1979ee678b4971dd74e99089558fe7d5faaa6f70a9fde42c13b5904660de5e3246ef83ebf8fd'
